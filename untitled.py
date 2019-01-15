6fd702b17e0a52da3c1f8341e4f6e64eafa42056
EricWWright <tddragonknight@gmail.com>
2019-01-15 10:13:56 -0700

    Changed eric.height to 1.72

--- untitled.py
+++ untitled.py
@@ -14,13 +14,13 @@ class Human(object):
     def discribe_self(self):
         print("I have", self.hair_color, "hair")
         print("I have", self.eye_color, "eyes")
-        print("I am", self.height, "tall")
+        print("I am", self.height, " m tall")
         print("I weigh", self.weight, "lbs")
         print("I am a", self.race, self.gender, "with an IQ of", self.iq)
     def bmi(self):
         """Calculates bmi for Human""" 
 
-eric = Human("Eric", "Black", "Brown", 5.8,
+eric = Human("Eric", "Black", "Brown", 1.72,
              195, 300, "Male", "White")
 eric.introduce_self()
 eric.discribe_self()
